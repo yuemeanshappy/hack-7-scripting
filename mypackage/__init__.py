7f8872e8be19a00132e6e71826cc8fb808abf75c
yue <yy3098@columbia.edu>
2025-02-23 21:46:52 -0500

    add an example pacakge and module

---
+++ mypackage/__init__.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+"""
+The mypackage package is used to learn about package filestructure.
+"""
+from . import mymodule
+
+# Here we use the convention from . to tell it where to the mymodule module is located, where . means the current directory.
